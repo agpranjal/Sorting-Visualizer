4bbf70a84165bb55cd620f3b80dcecd16d462d36
Pranjal <pranjal.ag.1999@gmail.com>
2020-10-04 13:25:00 +0530

    Merge sort + Insertion sort

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 import {SortingAlgorithms} from "./sorting-algorithms";
 
 class SortingVisualizer extends SortingAlgorithms {
     constructor(props) {
         super(props);
         this.state = {arr:[]};
-        this.ARRAY_LENGTH = 350;
+        this.ARRAY_LENGTH = 400;
         this.MIN_HEIGHT = 100;
         this.MAX_HEIGHT = 500;
-        this.ANIMATION_SPEED_MS = 10;
+        this.ANIMATION_SPEED_MS = 3;
+        this.arrayBarWidth = 2*window.innerWidth/1600; // calculate width of array bars based on avaiable width
     }
 
     componentDidMount() {
@@ -28,13 +28,153 @@ class SortingVisualizer extends SortingAlgorithms {
     }
 
 
+    bubbleSort = () => {
+        let animations = this.getBubbleSortAnimations();
+        let d = document.getElementsByClassName("array-bar");
+
+        for (let i=0; i<animations.length; i++) {
+            setTimeout(() => {
+                let [value1, index1, value2, index2] = animations[i];
+
+                // Set the height
+                d[index1].style.height = `${value1}px`;
+                d[index2].style.height = `${value2}px`;
+
+                // Set the color
+                d[index1].style.backgroundColor = "red";
+                d[index2].style.backgroundColor = "red";
+
+                // Change color back to normal after an interval
+                setTimeout(() => {
+                    d[index1].style.backgroundColor = "blue";
+                    d[index2].style.backgroundColor = "blue";
+                }, this.ANIMATION_SPEED_MS);
+
+            }, i*this.ANIMATION_SPEED_MS);
+
+        }
+    }
+
+    insertionSort = () => {
+        let animations = this.getInsertionSortAnimations();
+        let d = document.getElementsByClassName("array-bar");
+
+        for (let i=0; i<animations.length; i++) {
+            setTimeout(() => {
+                let [value1, index1, value2, index2] = animations[i];
+
+                // Set the height
+                d[index1].style.height = `${value1}px`;
+                d[index2].style.height = `${value2}px`;
+
+                // Set the color
+                d[index1].style.backgroundColor = "red";
+                d[index2].style.backgroundColor = "red";
+
+                // Change color back to normal
+                setTimeout(() => {
+                    d[index1].style.backgroundColor = "blue";
+                    d[index2].style.backgroundColor = "blue";
+                }, this.ANIMATION_SPEED_MS);
+
+            }, i*this.ANIMATION_SPEED_MS);
+
+        }
+    }
+
+    selectionSort = () => {
+        let animations = this.getSelectionSortAnimations();
+        let d = document.getElementsByClassName("array-bar");
+
+        for (let i=0; i<animations.length; i++) {
+            setTimeout(() => {
+                let [value1, index1, value2, index2] = animations[i];
+
+                // Set the height
+                d[index1].style.height = `${value1}px`;
+                d[index2].style.height = `${value2}px`;
+
+                // Set the color
+                d[index1].style.backgroundColor = "red";
+                d[index2].style.backgroundColor = "red";
+
+                // Change color back to normal after an interval
+                setTimeout(() => {
+                    d[index1].style.backgroundColor = "blue";
+                    d[index2].style.backgroundColor = "blue";
+                }, this.ANIMATION_SPEED_MS);
+
+            }, i*this.ANIMATION_SPEED_MS);
+
+        }
+    }
+
+    mergeSort = () => {
+        let animations = this.getMergeSortAnimations();
+        let d = document.getElementsByClassName("array-bar");
+
+        for (let i=0; i<animations.length; i++) {
+            setTimeout(() => {
+                let [value1, index1, value2, index2] = animations[i];
+
+                // Set the height
+                d[index1].style.height = `${value1}px`;
+
+                // Set the color
+                d[index1].style.backgroundColor = "red";
+
+                // Change color back to normal after an interval
+                setTimeout(() => {
+                    d[index1].style.backgroundColor = "blue";
+                }, this.ANIMATION_SPEED_MS);
+
+            }, i*this.ANIMATION_SPEED_MS);
+
+        }
+    }
+
+    heapSort = () => {
+        let animations = this.getHeapSortAnimations();
+        let d = document.getElementsByClassName("array-bar");
+
+        for (let i=0; i<animations.length; i++) {
+            setTimeout(() => {
+                let [value1, index1, value2, index2] = animations[i];
+                index1--;
+                index2--;
+
+                try {
+                    // Set the height
+                    d[index1].style.height = `${value1}px`;
+                    d[index2].style.height = `${value2}px`;
+                } catch (err) {
+                    console.log(index1, index2, d.length);
+                }
+
+                // Set the color
+                d[index1].style.backgroundColor = "red";
+                d[index2].style.backgroundColor = "red";
+
+                // Change color back to normal after an interval
+                setTimeout(() => {
+                    d[index1].style.backgroundColor = "blue";
+                    d[index2].style.backgroundColor = "blue";
+                }, this.ANIMATION_SPEED_MS);
+
+            }, i*this.ANIMATION_SPEED_MS);
+
+        }
+    }
+
+
     render() {
         return (
             <div>
-                {this.state.arr.map((h, index) => {
-                    return <div key={index} className="array-bar" style={{height: h+"px"}}></div>;
-                })}
-
+                <div id="array-container">
+                    {this.state.arr.map((h, index) => {
+                        return <div key={index} className="array-bar" style={{height: h+"px", width: this.arrayBarWidth+"px"}}></div>;
+                    })}
+                </div>
                 <div>
                     <br/><button type="button" onClick={this.resetArray}>Regenerate array</button><br/>
                     <button type="button" onClick={this.mergeSort}>Merge Sort</button>
@@ -50,8 +190,6 @@ class SortingVisualizer extends SortingAlgorithms {
         );
     }
 }
-
-
 
 function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
--- src/sorting-algorithms.js
+++ src/sorting-algorithms.js
@@ -7,79 +7,181 @@ class SortingAlgorithms extends React.Component {
         super(props);
     }
 
-    bubbleSort = () => {
-        let d = document.getElementsByClassName("array-bar");
-        let t = [...d];
+    getBubbleSortAnimations = () => {
+        let t = this.state.arr;
+        let animations = [];
 
-        for (let i=0; i<this.state.arr.length; i++) {
-            for (let j=0; j<this.state.arr.length-1-i; j++) {
-                let h1 = parseFloat(t[j].style.height);
-                let h2 = parseFloat(t[j+1].style.height);
-
-                if (h1 > h2) {
-                    t[j].style.height = h2+"px";
-                    t[j+1].style.height = h1+"px";
-
-                    setTimeout(() => {
-                        d[j].style.height = h2+"px";
-                        d[j+1].style.height = h1+"px";
-
-                    }, i*this.ANIMATION_SPEED_MS);
-                }
+        for (let i=0; i<t.length; i++) {
+            for (let j=0; j<t.length-1-i; j++) {
+                if (t[j] > t[j+1]) 
+                    [t[j], t[j+1]] = [t[j+1], t[j]];
+                
+                // Format: [value1, index1, value2, index2]
+                animations.push([t[j], j, t[j+1], j+1]);
             }
         }
+
+        return animations;
     }
 
-    insertionSort = () => {
-        let d = document.getElementsByClassName("array-bar");
-        let arr = [...d];
+
+    getInsertionSortAnimations = () => {
+        let arr = this.state.arr;
+        let animations = [];
 
         for (let i=0; i<arr.length-1; i++) {
             for (let j=i+1; j>0; j--) {
-                let h1 = parseFloat(arr[j].style.height);
-                let h2 = parseFloat(arr[j-1].style.height);
-
-                if (h1 > h2)
+                if (arr[j] > arr[j-1])
                     break;
 
                 [arr[j], arr[j-1]] = [arr[j-1], arr[j]];
-
-                setTimeout(() => {
-                    let x = d[j].style.height;
-                    d[j].style.height = d[j-1].style.height;
-                    d[j-1].style.height = x;
-                }, i*this.ANIMATION_SPEED_MS);
+                animations.push([arr[j], j, arr[j-1], j-1]);
             }
-
         }
+
+        return animations;
     }
 
-    selectionSort = () => {
-        let d = document.getElementsByClassName("array-bar");
-        let t = [...d];
+    getSelectionSortAnimations = () => {
+        let arr = this.state.arr;
+        let animations = [];
 
-        for (let i=0; i<t.length-1; i++) {
+        for (let i=0; i<arr.length-1; i++) {
             let u = i;
-            let min = parseFloat(t[i].style.height);
-            for (let j=i+1; j<t.length; j++) {
+            let min = arr[i];
 
-                let h = parseFloat(t[j].style.height);
-                if (h < min) {
-                    min = h;
+            for (let j=i+1; j<arr.length; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
                     u = j;
+                    animations.push([arr[i], i, arr[u], u]);
                 }
             }
 
-            setTimeout(() => {
-                let x = d[i].style.height;
-                d[i].style.height = d[u].style.height;
-                d[u].style.height = x;
-            }, i*this.ANIMATION_SPEED_MS);
+            [arr[i], arr[u]] = [arr[u], arr[i]];
+            animations.push([arr[i], i, arr[u], u]);
+        }
 
-            [t[i], t[u]] = [t[u], t[i]];
+        return animations;
+    }
+
+    mergeSortAndMerge = (arr, LB, UB, animations) => {
+        if (LB >= UB)
+            return
+        let mid = parseInt((UB+LB)/2);
+
+        this.mergeSortAndMerge(arr, LB, mid, animations);
+        this.mergeSortAndMerge(arr, mid+1, UB, animations);
+
+        // merge process
+        let N = (UB-LB)/2;
+        let temp = [];
+        let d = document.getElementsByClassName("array-bar");
+
+        // populate temp with null (N times)
+        for (let i=0; i<N; i++)
+            temp.push(null);
+
+        let i = LB, j = mid+1, k=0;
+        while (i <= mid && j <= UB) {
+            if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
+
+                i++;
+                k++;
+
+            } else {
+                temp[k] = arr[j];
+
+                k++;
+                j++;
+            }
+
+        }
+
+        while (i <= mid) {
+            temp[k] = arr[i];
+
+            i++;
+            k++;
+        }
+
+        while (j <= UB) {
+            temp[k] = arr[j];
+            k++;
+            j++;
+        }
+
+        for (let i=0; i<temp.length; i++) {
+            arr[LB+i] = temp[i];
+
+            animations.push([arr[LB+i], LB+i, -1, -1]);
         }
     }
 
+    getMergeSortAnimations = () => {
+        let arr = this.state.arr;
+        let animations = [];
+        this.mergeSortAndMerge(arr, 0, arr.length-1, animations);
+
+        return animations;
+    }
+
+    createHeap = (arr, animations) => {
+        for (let x=2; x<arr.length; x++) {
+            let i = x;
+
+            while (i > 1) {
+                let childIndex = parseInt(i/2);
+                if (arr[childIndex] < arr[i]) {
+                    [arr[childIndex], arr[i]] = [arr[i], arr[childIndex]];
+                    animations.push([arr[childIndex], childIndex, arr[i], i]);
+                    i = childIndex;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    deleteHeap = (arr, n, animations) => {
+        [arr[1], arr[n]] = [arr[n], arr[1]];
+        animations.push([arr[1], 1, arr[n], n]);
+        n--;
+
+        let i=1, j=2;
+
+        while (j <= n) {
+            if (j+1 <= n && arr[j] < arr[j+1])
+                j++;
+
+            if (arr[j] > arr[i]) {
+                [arr[i], arr[j]] = [arr[j], arr[i]];
+                animations.push([arr[i], i, arr[j], j]);
+                i = j;
+                j *= 2;
+            } else {
+                break;
+            }
+        }
+
+    }
+
+    getHeapSortAnimations = () => {
+        let animations = [];
+        let arr =  this.state.arr;
+        arr.unshift(null);
+        this.createHeap(arr, animations);
+
+        let i = arr.length-1;
+
+        while (i >= 1) {
+            this.deleteHeap(arr, i, animations);
+            i--;
+        }
+
+        return animations;
+    }
 }
 
 export {SortingAlgorithms};
